69e7c875bfee815d6bf6954135201dfb8a7650b6
Dominik Worosz <dominik.worosz.programista@gmail.com>
2021-10-13 13:10:46 +0200

    trying_to_fix_logging_system

--- funkcje.js
+++ funkcje.js
@@ -1,24 +1,25 @@
+var login;
+var haslo;
+
 function rejestracja()
-		{
-			var login = document.getElementById("login").value;
-			var haslo = document.getElementById("haslo").value;
-			
-			window.alert(login);
-			document.location='logowanie.html';
-		}
-		
+{
+		login=document.getElementById('login').value;
+		haslo=document.getElementById('haslo').value;
+		//alert(login);
+		document.location="logowanie.html";
+}
 function logowanie()
-		{
-			var login_podany = document.getElementById("login").value;
-			var haslo_podane = document.getElementById("haslo").value;
-			
-			
-			if(login_podany==login && haslo_podane==haslo)
+{
+		var login1=document.getElementById('login1').value;
+		var haslo1=document.getElementById('haslo1').value;
+		if( login == login1 && haslo == haslo1)
 			{
-				document.location='menu.html'
+				alert(login);
+				document.location='menu.html';
 			}
-			else 
+		else 
 			{
-				window.alert(login);
+				alert(login);
+				alert("Niepoprawny login lub hasło");
 			}
-		}
+}
