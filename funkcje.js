0344f11f42b40b2d86ad00df6995d4dd8afce92d
Dominik Worosz <dominik.worosz.programista@gmail.com>
2021-10-14 13:36:21 +0200

    balance_web_added

--- funkcje.js
+++ funkcje.js
@@ -1,6 +1,20 @@
 var login;
 var haslo;
 
+var dzisiaj = new Date();
+		
+var dzien = dzisiaj.getDate();
+var miesiac = dzisiaj.getMonth()+1;
+var rok = dzisiaj.getFullYear();
+
+var data=dzien + "-" + miesiac + "-" + rok;
+
+function ustawDate()
+{
+	document.getElementById('data').value= data;
+}
+
+
 function rejestracja()
 {
 		login=document.getElementById('login').value;
@@ -12,14 +26,32 @@ function logowanie()
 {
 		var login1=document.getElementById('login1').value;
 		var haslo1=document.getElementById('haslo1').value;
-		if( login == login1 && haslo == haslo1)
-			{
-				alert(login);
-				document.location='menu.html';
-			}
-		else 
-			{
-				alert(login);
-				alert("Niepoprawny login lub hasło");
-			}
+	
+		document.location='menu.html';
 }
+
+
+google.charts.load('current', {'packages':['corechart']});
+google.charts.setOnLoadCallback(drawChart);
+
+// Draw the chart and set the chart values
+function drawChart() {
+  var data = google.visualization.arrayToDataTable([
+  ['Task', 'Hours per Day'],
+  ['Jedzenie', 3000],
+  ['Mieszkanie', 1000],
+  ['Transport', 500],
+  ['Ksiązki', 200],
+  ['Dzieci', 100]
+]);
+
+  // Optional; add a title and set the width and height of the chart
+  var options = {'title':'Wydatki', 'width':550, 'height':400};
+
+  // Display the chart inside the <div> element with id="piechart"
+  var chart = new google.visualization.PieChart(document.getElementById('piechart'));
+  chart.draw(data, options);
+}
+
+
+
